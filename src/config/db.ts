a2fa79b59fb6ab1f8ad71a3a35c27a9747db20e1
Rao Fahad <raofahad046@gmail.com>
2024-08-16 16:57:32 +0500

    successfully did the server setup

---
+++ src/config/db.ts
@@ -0,0 +1,18 @@
+// ======================================================================
+// THIS FUNCITON IS RESPONSIBLE FOR CREATING A CONNECTION WITH MONGO_DB
+// ======================================================================
+
+import mongoose from "mongoose";
+
+const connectDB = async (): Promise<void> => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI as string);
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${(error as Error).message}`);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
