bfc043c3dd558231f31b7071da502c5f16f86df7
Rao Fahad <raofahad046@gmail.com>
2024-08-18 05:43:21 +0500

    created a user model

---
+++ src/models/User.ts
@@ -0,0 +1,26 @@
+import { Schema, model, Document } from 'mongoose';
+
+interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+  isVerified: boolean;
+  otp?: string;
+  otpExpiry?: Date;
+  refreshToken?: string;
+}
+
+const userSchema = new Schema<IUser>({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  isVerified: { type: Boolean, default: false },
+  otp: { type: String },
+  otpExpiry: { type: Date },
+  refreshToken: { type: String },
+}, {
+  timestamps: true,
+});
+
+const User = model<IUser>('User', userSchema);
+export default User;
