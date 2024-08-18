f88b5dde72d9f2c34da1521a620b56a55663efb2
Rao Fahad <raofahad046@gmail.com>
2024-08-19 00:56:06 +0500

    did some adjustements in event schema

--- src/models/Event.ts
+++ src/models/Event.ts
@@ -8,7 +8,7 @@ interface IEvent extends Document {
   title: string;
   description: string;
   date: Date;
-  seatsAvailable: number;
+  totalSeats: number;
   createdBy: Types.ObjectId;
   reservedSeats: number;
   reservedBy: Types.ObjectId[];
@@ -19,7 +19,7 @@ const eventSchema = new Schema<IEvent>(
     title: { type: String, required: true },
     description: { type: String, required: true },
     date: { type: Date, required: true },
-    seatsAvailable: { type: Number, required: true },
+    totalSeats: { type: Number, required: true },
     reservedSeats: { type: Number, default: 0 },
     createdBy: { type: Schema.Types.ObjectId, ref: "User", required: true },
     reservedBy: [{ type: Schema.Types.ObjectId, ref: "User" }],
