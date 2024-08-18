5bd81f43a29a50b583f4e126db26008dc67a9896
Rao Fahad <raofahad046@gmail.com>
2024-08-18 20:44:16 +0500

    created the auth routes

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -6,6 +6,10 @@ import express from "express";
 import registerUser from "../controllers/auth/register";
 import sendOtp from "../controllers/auth/sendOtp";
 import verifyUser from "../controllers/auth/verifyUser";
+import forgotPassword from "../controllers/auth/forgetPassword";
+import resetPassword from "../controllers/auth/reset-password";
+import loginUser from "../controllers/auth/login";
+import refreshAccessToken from "../controllers/auth/refreshAccessToken";
 const router = express.Router();
 
 // =======================================================================
@@ -18,12 +22,36 @@ router.post("/register", registerUser);
 // 2. SEND OTP (ONE TIME PASSWORD) ROUTE
 // =======================================================================
 
-router.post("/send-otp", sendOtp)
+router.post("/send-otp", sendOtp);
 
 // =======================================================================
 // 3. VERIFY USER ROUTE
 // =======================================================================
 
-router.post("/verify-user", verifyUser)
+router.post("/verify-user", verifyUser);
+
+// =======================================================================
+// 4. FORGET PASSWORD ROUTE
+// =======================================================================
+
+router.post("/forget-password", forgotPassword);
+
+// =======================================================================
+// 5. RESET PASSWORD ROUTE
+// =======================================================================
+
+router.post("/reset-password", resetPassword);
+
+// =======================================================================
+// 6. LOGIN ROUTE
+// =======================================================================
+
+router.post("/login", loginUser);
+
+// =======================================================================
+// 7. REFRESH ACCESS TOKEN ROUTE
+// =======================================================================
+
+router.post("/refresh-access-token", refreshAccessToken)
 
 export default router;
