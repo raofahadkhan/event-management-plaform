6b101ecdcd662004694b3baa6add082b70b84157
Rao Fahad <raofahad046@gmail.com>
2024-08-25 01:46:19 +0500

    created the login component

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -4,37 +4,26 @@ import React from "react";
 
 const Page = () => {
   return (
-    <div className="flex flex-col justify-center items-center h-screen bg-[#F8F8FA] gap-3">
+    <div className="flex flex-col justify-center items-center h-screen bg-[#F8F8FA] gap-7">
       <div className="flex flex-col text-center gap-2">
         <h1 className="text-2xl md:text-3xl font-bold leading-[43.67px]">
-          Sign Up to <span className="text-[#784BFA]">Event Hive</span>
+          Login to <span className="text-[#784BFA]">Event Hive</span>
         </h1>
       </div>
       <div className="w-[90%] md:w-1/2 lg:w-1/3">
         <form action="" className="flex flex-col gap-4">
-          <div className="flex flex-col gap-[10px]">
-            <Label htmlFor="name" value="Name" />
-            <Input name="name" type="text" placeholder="Enter your name" />
-          </div>
-
           <div className="flex flex-col gap-[10px]">
             <Label htmlFor="email" value="Email" />
             <Input name="email" type="email" placeholder="Enter your email" />
           </div>
 
           <div className="flex flex-col gap-[10px]">
-            <Label htmlFor="password" value="Password" />
+            <div className="flex justify-between">
+              <Label htmlFor="password" value="Password" />
+              <p className="text-[#7E7E7E] text-[14px] self-end cursor-pointer">Forgot your password?</p>
+            </div>
             <Input
               name="password"
-              type="password"
-              placeholder="Enter your password"
-            />
-          </div>
-
-          <div className="flex flex-col gap-[10px]">
-            <Label htmlFor="confirm-password" value="Confirm Password" />
-            <Input
-              name="confirm-password"
               type="password"
               placeholder="Enter your password"
             />
